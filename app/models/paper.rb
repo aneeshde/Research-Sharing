43f3e2ca13f7158ddcb8aad58f5f3d004f52e1cb
Mounica1291 <psai@seas.upenn.edu>
2014-11-20 15:23:39 -0600

    Paperclip removed

--- app/models/paper.rb
+++ app/models/paper.rb
@@ -12,7 +12,7 @@
 
 class Paper < ActiveRecord::Base
 
-  attr_accessible :author, :description, :title, :author_id, :author_attributes, :document
+  attr_accessible :author, :description, :title, :author_id, :author_attributes
   validates :title, :presence => true
   belongs_to :author
 
@@ -20,8 +20,8 @@ class Paper < ActiveRecord::Base
 
   #accepts_nested_attributes_for :uploads, :allow_destroy => true
 
-  has_attached_file :document,
-                   :url  => "/assets/upload/"
+  #has_attached_file :document,
+                   #:url  => "/assets/upload/"
                    # :path => ":rails_root/public/assets/products/:id/:style/:basename.:extension"
 
   #validates_attachment_content_type :document
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20141120034248) do
+ActiveRecord::Schema.define(:version => 20141117031117) do
 
   create_table "authors", :force => true do |t|
     t.string   "email",                  :default => "", :null => false
@@ -40,13 +40,9 @@ ActiveRecord::Schema.define(:version => 20141120034248) do
     t.string   "title"
     t.string   "author"
     t.text     "description"
-    t.datetime "created_at",            :null => false
-    t.datetime "updated_at",            :null => false
+    t.datetime "created_at",  :null => false
+    t.datetime "updated_at",  :null => false
     t.integer  "author_id"
-    t.string   "document_file_name"
-    t.string   "document_content_type"
-    t.integer  "document_file_size"
-    t.datetime "document_updated_at"
   end
 
   create_table "search_pages", :force => true do |t|
