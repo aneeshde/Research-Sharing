58694a83f4c5098fa96301495bb4dd88b3f327a2
Aneesh De <aneeshde@seas.upenn.edu>
2014-12-16 21:19:23 -0500

    Unable to reload content

--- app/controllers/faqs_controller.rb
+++ app/controllers/faqs_controller.rb
@@ -33,11 +33,6 @@ class FaqsController < ApplicationController
   def create
     @faq = Faq.new(params[:faq])
     @faq.paper_id = params[:paper_id]
-    #
-    # @faq.type1 = 0
-    # @faq.type1 = 1 if author_signed_in?
-
-
     @faq.save
     @paper = Paper.find(@faq.paper_id)
 
